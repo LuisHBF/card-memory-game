c7e06341b45b08a612dc26340c3f1a385f05a3b2
Luis Henrique Franco <luishbfranco@hotmail.com>
2020-03-30 02:48:50 -0300

    changing default cards

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ export function initialize(){
         new Card(new Icon('fa fa-home','#9c88ff'),'House'),
         new Card(new Icon('fab fa-angellist','#fbc531'),'Hey bro!'),
         new Card(new Icon('fas fa-apple-alt','#4cd137'),'Apple'),
-        new Card(new Icon('fas fa-angry','#e84118'),'I\'m mad!'),
+        new Card(new Icon('fas fa-basketball-ball','#e84118'),'basketball'),
         new Card(new Icon('fas fa-bath','#7f8fa6'),'Bath time!'),
         new Card(new Icon('fas fa-anchor','#00a8ff'),'Anchor')
     ];
