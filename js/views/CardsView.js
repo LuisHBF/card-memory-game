176407300389d7984877b9f741d27a01e9c1d09b
Luis Henrique Franco <luishbfranco@hotmail.com>
2020-03-30 00:40:44 -0300

    removing unused functions

--- js/views/CardsView.js
+++ js/views/CardsView.js
@@ -14,7 +14,7 @@ export class CardsView{
             this._cards.map(card => 
                 `
                     <div class="col-sm-6 col-md-3 p-4">
-                        <div class="card text-center" onclick="this.alertq()">
+                        <div class="card text-center">
                             <i class="${card.icon.icon}" style="color: ${card.icon.color}"></i>
                             <h3>${card.title}</h3>
                         </div>
@@ -31,7 +31,4 @@ export class CardsView{
         this._element.innerHTML = html;
     }
 
-    alertq(){
-        alert(1);
-    }
 }
